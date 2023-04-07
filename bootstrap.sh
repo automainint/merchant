7b3d044c3547044e066683bfa787d7632d5a4c43
Mitya Selivanov <automainint@guattari.tech>
2023-04-07 18:39:54 +0200

    gpg recv keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,9 @@ sudo nala install -y nginx certbot python3-certbot-nginx
 sudo nala install -y python3-pyqt5 libsecp256k1-0 python3-cryptography python3-setuptools python3-pip nodejs npm nginx certbot python3-certbot-nginx
 wget https://download.electrum.org/4.3.4/Electrum-4.3.4.tar.gz
 wget https://download.electrum.org/4.3.4/Electrum-4.3.4.tar.gz.asc
+gpg --keyserver keyserver.ubuntu.com --recv-keys 637DB1E23370F84AFF88CCE03152347D07DA627C 0EEDCFD5CAFB459067349B23CA9EEEC43DF911DC 6694D8DE7BE8EE5631BED9502BD5824B7F9470E6
 gpg --verify Electrum-4.3.4.tar.gz.asc
-if [ $? -ne 0 ]; then exit; fi
+if [ $? -ne 0 ]; then exit 1; fi
 sudo -H pip3 install Electrum-4.3.4.tar.gz
 
 # install monero
